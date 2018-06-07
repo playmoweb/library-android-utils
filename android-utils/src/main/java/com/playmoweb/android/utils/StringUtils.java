e7d7069803b333800223fcee69f3fdbca7d3bbba
Steve Grosbois <steve@playmoweb.com>
2018-06-07 17:16:34 +0200

    Added two new span methods to StringUtils

--- android-utils/src/main/java/com/playmoweb/android/utils/StringUtils.java
+++ android-utils/src/main/java/com/playmoweb/android/utils/StringUtils.java
@@ -3,7 +3,9 @@ package com.playmoweb.android.utils;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.Html;
+import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextUtils;
 
 import java.util.Collection;
 import java.util.regex.Matcher;
@@ -70,5 +72,40 @@ public class StringUtils {
         } else {
             return Html.fromHtml(text);
         }
+    }
+
+    /**
+     * Examples :
+     * - <code>span(new UnderlineSpan(), "all of this is underlined")</code>
+     * - <code>span(new StyleSpan(Typeface.BOLD), "all of this is bold")</code>
+     *
+     * @param spanToApply Object spanToApply
+     * @param all         CharSequence all
+     * @return SpannableString
+     */
+    @NonNull
+    public static SpannableString span(@NonNull final Object spanToApply, @NonNull final CharSequence all) {
+        return span(spanToApply, all, null);
+    }
+
+    /**
+     * Examples :
+     * - <code>span(new UnderlineSpan(), "only this is underlined", "this")</code>
+     * - <code>span(new StyleSpan(Typeface.BOLD), "only this is bold", "this")</code>
+     *
+     * @param spanToApply Object spanToApply
+     * @param all         CharSequence all
+     * @param needle      CharSequence needle
+     * @return SpannableString
+     */
+    @NonNull
+    public static SpannableString span(@NonNull final Object spanToApply, @NonNull final CharSequence all, @Nullable final CharSequence needle) {
+        SpannableString spannableString = new SpannableString(all);
+        if (needle != null) {
+            spannableString.setSpan(spanToApply, TextUtils.indexOf(all, needle), TextUtils.indexOf(all, needle) + needle.length(), 0);
+        } else {
+            spannableString.setSpan(spanToApply, 0, all.length(), 0);
+        }
+        return spannableString;
     }
 }
